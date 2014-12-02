c39346f21ad37f0588a386ad46cf1f1af7286f08
Flavio Silva <flv.alves@gmail.com>
2014-12-02 12:09:35 -0200

    criando classe abstrata para refatoração das futuras filhas, diminuindo a repetição de código

---
+++ src/DP/Form/AbstractForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DP\Form;
+
+abstract class AbstractForm
+{
+    protected $attributes = [] ;
+    protected $fields = [];
+    protected $label;
+
+    public function __construct($name)
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getName()
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setAttribute($name, $value)
+    {
+        $this->attributes[$name] = $value;
+        return $this;
+    }
+
+    public function createField(AbstractForm $field)
+    {
+        $this->fields[] = $field;
+
+        return $this;
+    }
+
+    abstract function render($name = null);
+} 
